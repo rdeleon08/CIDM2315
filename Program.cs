d9def178b5345703f2d01b6aa373ed1bf87edb96
rdeleon08 <112038207+rdeleon08@users.noreply.github.com>
2022-10-19 10:24:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework7;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer c1 = new Customer(110,"Alice",28);
+        Customer c2 = new Customer(111,"Bob",30);
+
+        c1.PrintCusInfo();
+        c2.PrintCusInfo();
+        Console.WriteLine();
+
+        c1.ChangeID(220);
+        c2.ChangeID(221);
+
+        c1.PrintCusInfo();
+        c2.PrintCusInfo();
+    }
+}
+
+class Customer{
+    private int cus_id;
+    public string cus_name;
+    public int cus_age;
+    public Customer(int id,string name,int age)
+    {
+        cus_id = id;
+        cus_name = name;
+        cus_age = age;
+    }
+    public void ChangeID(int new_id)
+    {
+        cus_id = new_id;
+    }
+    public void PrintCusInfo()
+    {
+        Console.WriteLine($"Customer ID:{0}, Customer Name:{1}, Customer Age:{2}", cus_id,cus_name,cus_age);
+    }
+}
